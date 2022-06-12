e3356b33fb1f576ba7455240c2548013701d622c
arindra97 <ar28297@gmail.com>
2022-06-12 22:45:14 +0700

    fix: challenge-07

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try{
+    // menggabungkan object film theater IXX dan VGC
+    const theater = (await promiseTheaterIXX()).concat(await promiseTheaterVGC());
+    // mencari jumlah emosi dari film yang ditonton oleh seseorang
+    let result = 0;
+    theater.map((element) => element.hasil === emosi && (result += 1));
+    return result;
+  } catch(err){
+    console.log(err);
+  }
+}
 
 module.exports = {
   promiseOutput,
